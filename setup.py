c808ce953ecd57e86b5a89e651e513a33e36ae5b
Usmanrajaa <ansariusman858@gmail.com>
2024-02-16 00:32:01 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'mohammad usman raja',
+    author_email= 'ansariusman858@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
